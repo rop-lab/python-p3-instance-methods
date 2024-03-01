f1ce61f12624aa52a6d860c59cc6f1017ef69223
cheruiyotrop <cheruiyot.rop@student.moringaschool.com>
2024-03-01 12:06:27 +0300

    completed

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,9 @@
-#!/usr/bin/env python3
-
 class Person:
-    # Class body goes here
+    def introduce(self):
+        print("Hello World!")
 
-    #Instance method definition
-    pass
+    def walk(self):
+        print("The person is walking.")
+
+    def talk(self):
+        print("Hello World!")
